4434db136b92b0cdebb6da52540998ff591fd7cc
NSmirnov9x <n.smirnov@itigris.ru>
2016-03-23 21:21:53 +0300

    Shops and workers generator

--- Amplifine/src/groovy/amplifine/gen/tables/ShopsGenerator.groovy
+++ Amplifine/src/groovy/amplifine/gen/tables/ShopsGenerator.groovy
@@ -2,24 +2,26 @@ package amplifine.gen.tables
 
 import amplifine.gen.MongoGenerator
 import groovy.transform.Canonical
+import mongodb.MongoDBUtil
 
 @Canonical
 class ShopsData {
     String name = null
-    String address = null
+    Map address = [:]
     String tel = null
 }
 
 class ShopsGenerator implements MongoGenerator {
 
     static ShopsData[] data = [
-            ["General Music Store", "Proletary street, 20, 1", "88135557070"],
-            ["North Cither", "Line-3 ave, 11, 345-5", "88125550002"],
-            ["Syntesis", "Radical street, 1, 110-2", "88125553412"],
-            ["Mad Guitars", "Round ave, 45-2, 1", "88125559987"],
-            ["Music Stuff", "Line-2 ave, 123, 32A", "012"],
-            ["Classique", "Central street, 1, 1B", "88125550001"],
-            ["Deep Bass", "Lowpass street, 4, 22", "88125550405"]
+            [name: "General Music Store", address: [city: "Calimport", street: "Proletary street", house: "20, 1"], tel: "88135557070"],
+            [name: "North Cither", address: [city: "Waterdeep", street: "Line-3 ave", house: "11, 345-5"], tel: "88125550002"],
+            [name: "Syntesis", address: [city: "Boomtown", street: "Radical street 1", house: "110-2"], tel: "88125553412"],
+            [name: "Mad Guitars", address: [city: "Calimport", street: "Round ave", house: "45-2"], tel: "88125559987"],
+            [name: "Music Stuff", address: [city: "City 17", street: "Line-2 ave", house: "123-32A"], tel: "012"],
+            [name: "Classique", address: [city: "Detroit", street: "Central street", house: "1-B"], tel: "88125550001"],
+                [name: "Deep Bass", address: [city: "City 17", street: "Lowpass street", house: "4-22"], tel: "88125550405"],
+            [name: "Sweet Sound House", address: [city: "Northumberland", street: "Lowland ave", house: "1-88"], tel: "+3634325550405"]
     ]
 
     static int getMaxPossibleRecords() {
@@ -27,6 +29,25 @@ class ShopsGenerator implements MongoGenerator {
     }
 
     Boolean insertAll() {
+        def db = MongoDBUtil.getDB()
+        db.getCollection("shops").drop()
 
+        def record
+
+        Boolean status = true
+        for (result in data) {
+            record = [:]
+
+            record << [name: result.name]
+            record << [address: result.address]
+            record << [tel: result.tel]
+
+            status = (db.getCollection("shops") << record)
+            if (!status) {
+                break
+            }
+        }
+
+        return status
     }
 }
---
+++ Amplifine/src/groovy/amplifine/gen/tables/WorkersGenerator.groovy
@@ -0,0 +1,99 @@
+package amplifine.gen.tables
+
+import groovy.transform.Canonical
+
+import amplifine.gen.MongoGenerator
+import mongodb.MongoDBUtil
+
+@Canonical
+class WorkersData {
+    String name
+    String surname
+
+    Map documents
+
+    String city
+
+    Date birthday
+    Date fired
+}
+
+class WorkersGenerator implements MongoGenerator {
+
+    def data = []
+
+    int getMaxPossibleRecords() {
+        return data.size()
+    }
+
+    WorkersGenerator() {
+        def names = [
+                "Anna", "Mark", "Maria", "Roman", "John", "Ching", "Andrew",
+                "Nicko", "Inna", "Aaron", "Shy", "Christie", "Olga"
+        ]
+
+        def surnames = [
+                "Chong", "Goldman", "Kaufman", "Brown", "Smith", "Barrel",
+                "Finetuned", "Wellspeached", "Badwheel", "Goodman", "Niceman",
+                "Lowlifeman", "Richguy", "Ross", "Crow", "Yammamotoe"
+        ]
+
+        def cities = [
+                "Northumberland", "City 17", "Detroit", "Calimport", "Boomtown",
+                "Waterdeep", "Empire Bay", "Crew City", "Los-Gattos", "San-Clarentino"
+        ]
+
+        Random rn = new Random()
+
+        surnames.each { val ->
+            (1..rn.nextInt(3)).each {
+                def genCity = cities[rn.nextInt(cities.size())]
+
+                data << [
+                        name: names[rn.nextInt(names.size())],
+                        surname: val,
+                        city: genCity,
+                        documents: [
+                                passport: (rn.nextInt(325) + 4270).toString() + " in " + genCity,
+                                inn: (rn.nextInt(665) + 1234270),
+                                workerBook: (rn.nextInt(22) + 1000)
+                        ],
+                        birthday: new Date(rn.nextInt(20) + 1970, rn.nextInt(12), rn.nextInt(30)),
+                        fired: rn.nextInt(1000) > 333 ?
+                                new Date(rn.nextInt(5) + 2009, rn.nextInt(12), rn.nextInt(30)) : null,
+                ]
+            }
+        }
+
+        Collections.shuffle(data, rn)
+
+    }
+
+    Boolean insertAll() {
+        def db = MongoDBUtil.getDB()
+        db.getCollection("workers").drop()
+
+        def record
+
+        Boolean status = true
+        for (result in data) {
+            record = [:]
+
+            record << [name: result.name]
+            record << [surname: result.surname]
+            record << [documents: result.documents]
+            record << [city: result.city]
+            record << [birthday: result.birthday]
+
+            if (result.fired)
+                record << [fired: result.fired]
+
+            status = (db.getCollection("workers") << record)
+            if (!status) {
+                break
+            }
+        }
+
+        return status
+    }
+}
