c52646ecbf5b8af8a045a693beaf943907f81d7e
Komarevtsev Roman <hellromul@gmail.com>
2016-04-21 01:07:50 +0300

    Починил Левенштайна

--- Amplifine/src/groovy/amplifine/utils/SearchUtil.groovy
+++ Amplifine/src/groovy/amplifine/utils/SearchUtil.groovy
@@ -4,8 +4,6 @@ import com.mongodb.client.FindIterable
 import com.mongodb.client.MongoCollection
 import mongodb.MongoDBUtil
 import org.apache.commons.lang.StringUtils
-import org.bson.Document
-import org.bson.types.ObjectId
 
 class SearchUtil {
 
@@ -34,55 +32,12 @@ class SearchUtil {
         return replaceLatinChars(str)
     }
 
-    /**
-     * Функция полнотекстового поиска с изменением раскладки.
-     * Правило поиска - если было найдено менее 1% от общего размера базы, пробуем искать в другой раскладке
-     * При этом если результирующий для другой раскладки больше, используем его в качестве результата
-     * @param collection
-     * @param pattern
-     * @param limit
-     * @param offset
-     * @return Результат поиск
-     */
-    public static Object fullTextSearch(String collection, String pattern, int limit, int offset) {
-        //        SearchResult result = new SearchResult()
-
-        Closure requestSearch = { String text ->
-            return ['$text': ['$search': text]]
-        }
-
-        Map requestMeta = ['$score': ['$meta': 'textScore']]
-
-        MongoCollection salesCollection = MongoDBUtil.DB.getCollection(collection)
-        int collectionSize = salesCollection.count()
-
-        // Делаем запрос с исходной раскладкой
-        def search = pattern
-        FindIterable queryResult = salesCollection.find(requestSearch(search))
-
-        //def searchCyrillic = replaceLatinChars(pattern)
-
-        //def resultCyrillic = salesCollection.find(findCommandCyrillic, projectCommand).skip(offset).limit(limit).asList()
-
-        def returnList = []
-        def returnPattern = ""
-
-//        if (result || resultCyrillic) {
-//            returnList = (result.size() > resultCyrillic.size() ? result : resultCyrillic)
-//            returnPattern = (result.size() > resultCyrillic.size() ? search : searchCyrillic)
-//        }
-
-        //def totalSize = salesCollection.find((result.size() > resultCyrillic.size() ? findCommand : findCommandCyrillic)).size()
-
-        return [list: returnList, pattern: returnPattern, size: 5]
-    }
-
     public static SearchResult search(String collectionName, String query, int limit, int page) {
         MongoCollection collection = MongoDBUtil.DB.getCollection(collectionName)
 
         FindIterable iterator = collection.find()
 
-        final int LEVENSTEIN_THRESHOLD = 2
+        double SIMILARITY_THRESHOLD = 0.8
 
         int skip = limit * page
         int balance = 1
@@ -94,9 +49,8 @@ class SearchUtil {
 
             // Выходим, если уже хватит
             if (limit == 0) break
-
             // Весовое значение для оценки релевантности
-            int weight = 0
+            double weight = 0
 
             // Перебираем все поля объекта коллекции
             for (Map.Entry field in it) {
@@ -106,20 +60,20 @@ class SearchUtil {
                     // Найти все комбинации сравнений со словами запроса
                     for (word in query.split("\\s")) {
                         // Ищем расстояние через левенштейна для исходной раскладки...
-                        int len = FuzzySearch.levensteinDistance(fieldWord.toLowerCase(), word.toLowerCase())
+                        double sim = FuzzySearch.levensteinSimilarity(fieldWord.toLowerCase(), word.toLowerCase())
                         // ...и для альтернативной
-                        int lenAlt = FuzzySearch.levensteinDistance(fieldWord.toLowerCase(), replaceAuto(word).toLowerCase())
+                        double simAlt = FuzzySearch.levensteinSimilarity(fieldWord.toLowerCase(), replaceAuto(word).toLowerCase())
                         // Выбрав наикротчайший вариант, проверяем по порогу
-                        int min = 0
-                        if (len < lenAlt) {
-                            min = len
+                        double max
+                        if (sim >= simAlt) {
+                            max = sim
                             ++balance
                         } else {
-                            min = lenAlt
+                            max = simAlt
                             --balance
                         }
-                        if (min <= LEVENSTEIN_THRESHOLD) {
-                            weight += LEVENSTEIN_THRESHOLD - min
+                        if (max > SIMILARITY_THRESHOLD) {
+                            weight += max
                         }
                     }
                 }
@@ -140,80 +94,5 @@ class SearchUtil {
         result.realPattern = balance > 0 ? query : replaceAuto(query)
 
         return result
-    }
-
-    public static Object regexTextSearch(String pattern, Integer offset) {
-        def salesCollection = MongoDBUtil.DB.getCollection("sales")
-
-        def search = replaceCyrillicChars(pattern)
-        def searchCyrillic = replaceLatinChars(pattern)
-
-        def searchMap = ["goods.description": [$regex: '^' + search, $options: 'i']]
-        def searchMapCyrillic = ["goods.description": [$regex: '^' + searchCyrillic, $options: 'i']]
-
-        def result = salesCollection.find(searchMap).skip(offset).limit(LIMIT).asList()
-        def resultCyrillic = salesCollection.find(searchMapCyrillic).skip(offset).limit(LIMIT).asList()
-
-        def returnList = []
-        def returnPattern = ""
-
-        if (result || resultCyrillic) {
-            returnList = (result.size() > resultCyrillic.size() ? result : resultCyrillic)
-            returnPattern = (result.size() > resultCyrillic.size() ? search : searchCyrillic)
-        }
-
-        def totalSize = salesCollection.find((result.size() > resultCyrillic.size() ? searchMap : searchMapCyrillic)).size()
-
-        return [list: returnList, pattern: returnPattern, size: totalSize]
-    }
-
-    public static Map getGoodDescription(ObjectId id) {
-        def goodsCollection = MongoDBUtil.DB.getCollection("goods")
-        Map good = [:]
-
-        if (goodsList && goodsList.size() > 0) {
-            good = goodsList.find { it._id == id }
-        }
-
-        if (!good) {
-            def query = new Document("_id", new ObjectId(id.toString()))
-            good = goodsCollection.findOne(query)
-
-            if (good) {
-                goodsList << good
-            }
-        }
-
-        return good
-    }
-
-    public static List<Map> getPaginateMap(Integer offset, Long collectionSize) {
-        def pages = (collectionSize / LIMIT).toInteger()
-        def currentPage = (offset / LIMIT).toInteger() + 1
-
-        def paginateList = []
-
-        def firstPage = [num: 1, offset: 0]
-
-        paginateList << firstPage
-
-        if (currentPage - 2 <= 1) {
-            for (def i = currentPage; i > 1; i--) {
-                def newPage = [num: i, offset: ((i - 1) * LIMIT)]
-                paginateList << newPage
-            }
-        } else {
-            for (def i = currentPage - 2; i <= currentPage; i++) {
-                def newPage = [num: i, offset: ((i - 1) * LIMIT)]
-                paginateList << newPage
-            }
-        }
-
-        for (def i = currentPage + 1; i < pages && i <= currentPage + 2; i++) {
-            def newPage = [num: i, offset: ((i - 1) * LIMIT)]
-            paginateList << newPage
-        }
-
-        return paginateList
     }
 }
---
+++ Amplifine/src/java/amplifine/utils/FuzzySearch.java
@@ -0,0 +1,137 @@
+package amplifine.utils;
+
+import java.util.Arrays;
+
+public class FuzzySearch {
+    private static double threshold = 0.7;
+
+    public static double jaroWinklerSimilarity(String s1, String s2) {
+        int[] mtp = matches(s1, s2);
+        float m = mtp[0];
+        if (m == 0) {
+            return 0f;
+        }
+        float j = ((m / s1.length() + m / s2.length() + (m - mtp[1]) / m)) / 3;
+        float jw = j < threshold ? j : j + Math.min(0.1f, 1f / mtp[3]) * mtp[2] * (1 - j);
+        return jw;
+    }
+
+    private static int[] matches(String s1, String s2) {
+        String max, min;
+        if (s1.length() > s2.length()) {
+            max = s1;
+            min = s2;
+        } else {
+            max = s2;
+            min = s1;
+        }
+        int range = Math.max(max.length() / 2 - 1, 0);
+        int[] matchIndexes = new int[min.length()];
+        Arrays.fill(matchIndexes, -1);
+        boolean[] matchFlags = new boolean[max.length()];
+        int matches = 0;
+        for (int mi = 0; mi < min.length(); mi++) {
+            char c1 = min.charAt(mi);
+            for (int xi = Math.max(mi - range, 0),
+                 xn = Math.min(mi + range + 1, max.length()); xi < xn; xi++) {
+                if (!matchFlags[xi] && c1 == max.charAt(xi)) {
+                    matchIndexes[mi] = xi;
+                    matchFlags[xi] = true;
+                    matches++;
+                    break;
+                }
+            }
+        }
+        char[] ms1 = new char[matches];
+        char[] ms2 = new char[matches];
+        for (int i = 0, si = 0; i < min.length(); i++) {
+            if (matchIndexes[i] != -1) {
+                ms1[si] = min.charAt(i);
+                si++;
+            }
+        }
+        for (int i = 0, si = 0; i < max.length(); i++) {
+            if (matchFlags[i]) {
+                ms2[si] = max.charAt(i);
+                si++;
+            }
+        }
+        int transpositions = 0;
+        for (int mi = 0; mi < ms1.length; mi++) {
+            if (ms1[mi] != ms2[mi]) {
+                transpositions++;
+            }
+        }
+        int prefix = 0;
+        for (int mi = 0; mi < min.length(); mi++) {
+            if (s1.charAt(mi) == s2.charAt(mi)) {
+                prefix++;
+            } else {
+                break;
+            }
+        }
+        return new int[]{matches, transpositions / 2, prefix, max.length()};
+    }
+
+    public static double levensteinDistance(String s1, String s2) {
+        return (double)levensteinDistanceMetric(s1, s2) / (double)Math.max(s1.length(), s2.length());
+    }
+
+    public static double levensteinSimilarity(String s1, String s2) {
+        return 1.0 - levensteinDistance(s1, s2);
+    }
+
+    public static int levensteinDistanceMetric(String s1, String s2) {
+        if (s1.equals(s2)){
+            return 0;
+        }
+
+        if (s1.length() == 0) {
+            return s2.length();
+        }
+
+        if (s2.length() == 0) {
+            return s1.length();
+        }
+
+        // create two work vectors of integer distances
+        int[] v0 = new int[s2.length() + 1];
+        int[] v1 = new int[s2.length() + 1];
+        int[] vtemp;
+
+        // initialize v0 (the previous row of distances)
+        // this row is A[0][i]: edit distance for an empty s
+        // the distance is just the number of characters to delete from t
+        for (int i = 0; i < v0.length; i++) {
+            v0[i] = i;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            // calculate v1 (current row distances) from the previous row v0
+            // first element of v1 is A[i+1][0]
+            //   edit distance is delete (i+1) chars from s to match empty t
+            v1[0] = i + 1;
+
+            // use formula to fill in the rest of the row
+            for (int j = 0; j < s2.length(); j++) {
+                int cost = (s1.charAt(i) == s2.charAt(j)) ? 0 : 1;
+                v1[j + 1] = Math.min(
+                        v1[j] + 1,              // Cost of insertion
+                        Math.min(
+                                v0[j + 1] + 1,  // Cost of remove
+                                v0[j] + cost)); // Cost of substitution
+            }
+
+            // copy v1 (current row) to v0 (previous row) for next iteration
+            //System.arraycopy(v1, 0, v0, 0, v0.length);
+
+            // Flip references to current and previous row
+            vtemp = v0;
+            v0 = v1;
+            v1 = vtemp;
+
+        }
+
+        return v0[s2.length()];
+    }
+}
